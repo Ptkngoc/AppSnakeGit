c50a3dd5137d5f40aa16490574b905fb71329cc8
Ptkngoc <phamthikimngoc29052001@gmail.com>
2024-07-19 14:09:34 +0700

    Initial Commit

---
+++ AppGitSnakes/AppGitSnakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppGitSnakesApp.swift
+//  AppGitSnakes
+//
+//  Created by Ngọc Kim on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppGitSnakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppGitSnakes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppGitSnakes
+//
+//  Created by Ngọc Kim on 19/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
